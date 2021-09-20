374b09b0f7c5c94099a7cf8775266b51dfd895cc
Michael Mello <michael.mello@gmail.com>
2021-09-19 20:34:53 -0400

    Add repository method to take spec

--- src/Mello.SharedKernel/Interfaces/IAsyncRepository.cs
+++ src/Mello.SharedKernel/Interfaces/IAsyncRepository.cs
@@ -2,8 +2,8 @@ namespace Mello.SharedKernel.Interfaces;
 
 public interface IAsyncRepository<T, TId> where T : Entity<TId>, IAggregateRoot
 {
-    Task<T> GetByIdAsync(TId id);
-    Task<List<T>> GetAllAsync();
+    Task<T> FindByIdAsync(TId id);
+    Task<List<T>> FindAsync(ISpecification<T> specification);
     Task<T> AddAsync(T entity);
     Task UpdateAsync(T entity);
     Task DeleteAsync(T entity);
